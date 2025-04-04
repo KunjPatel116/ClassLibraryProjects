f16aae19df988fa26d1d04837f6d4fc6aae8488c
Kunj Patel <kpat1045@my.centennialcollege.ca>
2025-04-03 20:02:58 -0400

    Added console app consuming StringLibrary package

--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -1,17 +1,14 @@
-﻿// In ShowCase/Program.cs
+﻿// Program.cs in your Console App
 using static UtilityLibraries.StringLibrary;
-using UtilityLibraries;
 
-Console.WriteLine("String Library Demo");
-Console.WriteLine("------------------");
-
-TestString("Hello World");
-TestString("hello world");
-TestString(null);
-TestString("");
-
-void TestString(string input)
+class Program
 {
-    bool isUpper = input.StartsWithUpper();
-    Console.WriteLine($"\"{input}\" starts with uppercase? {(isUpper ? "YES" : "NO")}");
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+
+        bool startsWithUpper = input.StartsWithUpper();
+        Console.WriteLine($"Starts with uppercase: {startsWithUpper}");
+    }
 }
